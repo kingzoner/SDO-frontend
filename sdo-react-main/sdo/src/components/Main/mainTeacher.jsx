cbeeb111598e33a12b297ca7dabb70ef19e2eec7
Khalilov Kurbon <khalilovbobo7@gmail.com>
2026-03-15 13:49:27 +0300

    Исправлена навигация дисциплин в кабинете преподавателя Исправлена некликабельность выполненных заданий студента

--- sdo-react-main/sdo/src/components/Main/mainTeacher.jsx
+++ sdo-react-main/sdo/src/components/Main/mainTeacher.jsx
@@ -1,21 +1,39 @@
-import React from "react";
-import styled from 'styled-components'
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
 const Section = styled.section`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 40px 0px 245px;
-`
+ display: flex;
+ flex-direction: column;
+ align-items: center;
+ padding: 40px 0px 245px;
+`;
+
 const MainTeacher = () => {
-    return ( 
-      <>
+
+    const navigate = useNavigate();
+
+    const disciplines = [
+        { id: 1, name: "Математика" },
+        { id: 2, name: "Физика" },
+        { id: 3, name: "Программирование" }
+    ];
+
+    return (
         <Section>
-          <h1>
-            Страница Препода
-          </h1>
+
+            <h1>Страница Препода</h1>
+
+            {disciplines.map(discipline => (
+                <button
+                    key={discipline.id}
+                    onClick={() => navigate(`/laboratory/${discipline.id}`)}
+                >
+                    {discipline.name}
+                </button>
+            ))}
+
         </Section>
-      </>
     );
-}
+};
 
 export default MainTeacher;
